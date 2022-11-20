89aaff5eb1dbfb4c9f4ec7543b4e1c7a07b3411e
pavez69 <jpavezcarrillo23@gmail.com>
2022-11-20 03:39:25 -0300

    - Nav pestañas que ocupara el cliente - Responsive para celulares - useState manejo de estados

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Header } from "./components/Header";
-
+import { Hero } from './components/Hero'
 function App() {
   return (
     <div>
       <Header/>
+      <Hero/>
       
       </div>
     
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import { RiSunLine } from "react-icons/ri";
+import React, {useState} from 'react'
+import { RiSunLine, RiMenu3Fill, RiCloseLine } from "react-icons/ri";
 
-export const Header = () => {
+export  const Header = () => {
+
+   const [showMenu, setShowMenu] = useState(false)
   return (
-    <header className='flex items-center w-full p-4'>
-   <div className=' w-1/6 text-center'>
+    <header className='flex items-center justify-between xl:justify-start w-full p-4 h-[10-uvh] z-50'>
+   <div className=' xk:w-1/6 text-center'>
 
-    <h1 className='text-2xl font-bold'>Floreria SunShine <span className='text-primary text-5xl'>.</span><RiSunLine/></h1>
+    <a href='#' className='text-2xl font-bold relative p-1 bg-white'>
+      Floreria SunShine<span className='text-primary text-5xl'>.</span>{""}
+      <RiSunLine className='absolute -left-3 -botton-3 text-primary -z-10'/>
+      </a>
    </div>
-        <nav className=' flex-1 text-center'>
-            Nav
+        <nav 
+      className={`fixed bg-emerald-400 w-[80%] md:w-[40%] xl:w-full h-full ${
+        showMenu ? "left-0" : "-left-full"
+      } top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500 z-50`}
+    >
+      
+          <a href='#' className=''>
+            Encuentranos</a>
+            <a href='#' className=''>
+            Contacto</a>
+            <a href='#' className=''>
+            Flores</a>
+            <a href='#' className=''>
+            Creadora</a>
+            <a href='#' className=''>
+            Social</a>
+            
         </nav>
+
+        <button onClick={()=> setShowMenu(!showMenu)}className='text-2xl p-2'>
+          <RiMenu3Fill />
+          </button>
     </header>
   )
 }
